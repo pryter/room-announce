914ab4bb845f0510a01f776358b37245b84549b9
Peter <peterphongpak@gmail.com>
2021-05-19 00:12:21 +0700

    Add Desktop ui splash image

--- src/components/app/Display/DataRow.tsx
+++ src/components/app/Display/DataRow.tsx
@@ -2,11 +2,11 @@ import classnames from "classnames"
 
 export const DataRow = ({data}) => {
   return (
-    <div className={classnames(!(typeof data.title === "string" && typeof data.context !== "string") ? "flex space-x-2" : "space-y-1.5")}>
+    <div className={classnames((typeof data.title === "string" && typeof data.context !== "string") ? "space-y-1.5" : "flex space-x-2")}>
       {typeof data.title !== "string" ? data.title.map((item, index) => {
-        return <div key={`child-${index}`}>
+        return <div className="flex" key={`child1-${index}`}>
           <h1 className="text-gray-700 font-medium">{item}</h1>
-          <h1 className="text-TUCMC-gray-600 px-1.5">{data.context[index]}</h1>
+          <h1 className="text-TUCMC-gray-600 ml-2.5 mr-1.5">{data.context[index]}</h1>
         </div>
       }): <>
         <h1 className="text-gray-700 font-medium">{data.title}</h1>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,20 +9,28 @@ import {updateTaskfromSection} from "../hooks/index/utils";
 import {TaskProvider} from "../contexts/task";
 import {Credentials} from "@components/app/Credentials";
 import Display from "@components/app/Display";
+import Image from "next/image"
 
 export default function Index() {
 
   return (
     <TaskProvider>
-      <div className="py-12 space-y-6 px-6 max-w-[405px] mx-auto">
-        <div className="space-y-10">
-          <Heading/>
-          <StatusBox/>
-        </div>
-        <StudentID/>
-        <Credentials/>
-        <Display/>
-      </div>
+     <div className="md:flex">
+       <div className="flex-shrink hidden md:block w-[431px] relative min-h-screen">
+         <Image src="/assets/images/splash.jpg" layout="fill" className="object-cover"/>
+       </div>
+       <div className="md:mx-auto md:px-6">
+         <div className="pt-12 pb-20 space-y-6 px-6 max-w-[405px] mx-auto md:min-w-[405px] md:mt-20">
+           <div className="space-y-10">
+             <Heading/>
+             <StatusBox/>
+           </div>
+           <StudentID/>
+           <Credentials/>
+           <Display/>
+         </div>
+       </div>
+     </div>
     </TaskProvider>
   )
 }
