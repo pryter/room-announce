78e21623baeb8e7bb77dd06553420a12b512fd7a
Peter <peterphongpak@gmail.com>
2021-05-21 01:57:50 +0700

    Fix disabled input text color

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -9,10 +9,10 @@ const Input = ({value = "", updateHandler, placeholder = "", disabled = false, r
         onChange={(event) => {
           updateHandler(event.target.value)
         }}
-        className={classnames("appearance-none outline-none focus:ring-TUCMC-pink-500 focus:border-TUCMC-pink-500 block w-full rounded-full px-4 h-11 shadow-sm border-gray-300 placeholder-TUCMC-gray-400", className, disabled && "cursor-not-allowed")}
+        className={classnames("appearance-none outline-none focus:ring-TUCMC-pink-500 focus:border-TUCMC-pink-500 block w-full rounded-full px-4 h-11 shadow-sm border-gray-300 placeholder-TUCMC-gray-400", className, disabled && "cursor-not-allowed pointer-events-none")}
         placeholder={placeholder}
         value={value}
-        disabled={disabled}
+        readOnly={disabled}
         required={required}
       />
     </div>
