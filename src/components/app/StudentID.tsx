0800983dc6f470f3ccc15ecb0b39d96031920de0
Peter <peterphongpak@gmail.com>
2021-06-05 01:51:22 +0700

    Fix placeholder

--- src/components/app/StudentID.tsx
+++ src/components/app/StudentID.tsx
@@ -52,7 +52,7 @@ export const StudentID = ({updateCred, report}) => {
     <ContentBox className="flex justify-center items-center py-10 mt-6">
       <form onSubmit={submitFunc}>
         <div className="relative">
-          <input
+          {scheduled(<input
             type="text"
             onChange={(event) => {
               setStdID(event.target.value)
@@ -61,9 +61,20 @@ export const StudentID = ({updateCred, report}) => {
               setWarning(false)
             }}
             className={classnames("appearance-none outline-none block w-56 rounded-full px-4 h-11 placeholder-TUCMC-gray-400", warning ? "border-red-500 focus:ring-TUCMC-red-500 focus:border-TUCMC-red-500" : "border-gray-300 focus:ring-TUCMC-pink-500 focus:border-TUCMC-pink-500")}
-            placeholder={scheduled("เลขประจำตัวนักเรียน","เลขประจำตัวสอบ / นักเรียน")}
+            placeholder="เลขประจำตัวนักเรียน"
             required
-          />
+          />, <input
+            type="text"
+            onChange={(event) => {
+              setStdID(event.target.value)
+            }}
+            onFocus={() => {
+              setWarning(false)
+            }}
+            className={classnames("appearance-none outline-none block w-56 rounded-full px-4 h-11 placeholder-TUCMC-gray-400", warning ? "border-red-500 focus:ring-TUCMC-red-500 focus:border-TUCMC-red-500" : "border-gray-300 focus:ring-TUCMC-pink-500 focus:border-TUCMC-pink-500")}
+            placeholder="เลขประจำตัวสอบ / นักเรียน"
+            required
+          />)}
           <Button type="submit"
                   disabled={loading}
                   className={classnames("absolute w-11 h-11 rounded-full flex items-center justify-center top-0 right-0 cursor-pointer", loading ? "" : "bg-TUCMC-pink-400")}>
